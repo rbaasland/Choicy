97b56796ad7fba416d5c044acd9a159f480b36bd
Ron Baasland <ron.baasland@gmail.com>
2017-02-02 21:00:13 -0500

    Base unit test for choice list class

--- app/src/test/java/rbaasland/com/choicy/ChoiceListUnitTest.java
+++ app/src/test/java/rbaasland/com/choicy/ChoiceListUnitTest.java
@@ -1,4 +1,27 @@
 package rbaasland.com.choicy;
 
+import org.junit.Before;
+import org.junit.Test;
+import rbaasland.com.choicy.core.ChoiceList;
+import static junit.framework.Assert.assertEquals;
+
 public class ChoiceListUnitTest {
+    ChoiceList choices;
+
+    @Before
+    public void initialize() {
+        choices = new ChoiceList();
+    }
+
+    @Test
+    public void title_isCorrect() throws Exception {
+        choices.setListTitle("Restaurant List");
+
+        assertEquals(choices.getListTitle(), "Restaurant List");
+    }
+
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
 }
